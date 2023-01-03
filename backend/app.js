db091eece8f1d69be362585e404ff12e16e363d8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-03 15:41:35 +0530

    craete a home route

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
 import express from 'express'
 
 import connect from './config/db.js'
+import router from './routes/employe.route.js'
 
 // connect()
 
-
 const app=express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.get('/',(req, res) =>
-{
-    res.send("Hello World")
-})
+app.use('/api/v1',router)
 
 export default app
